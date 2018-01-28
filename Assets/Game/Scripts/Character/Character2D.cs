9ab142c2a2a8f6ab909d9f9e99a468bbfb2882f8
Jose Monge <jmonge@headlesschickengames.com>
2018-01-28 12:22:43 -0600

    Inventory and More
    Some fixes

--- Assets/Game/Scripts/Character/Character2D.cs
+++ Assets/Game/Scripts/Character/Character2D.cs
@@ -4,44 +4,66 @@ using UnityEngine;
 
 public class Character2D : MonoBehaviour {
 
-    private Vector3 targetPosition;
+    [System.NonSerialized]
+    public Vector3 targetPosition;
 
     public float speed;
 
     public Animator characterAnimator;
     private bool walking = false;
+
+
+
+    public GameObject Inventory;
+    public bool InventoryOpen;
 	// Use this for initialization
 	void Start () {
         targetPosition = this.transform.position;
        characterAnimator = GetComponent<Animator>();
+        InventoryOpen = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (Input.GetMouseButtonDown(0) && !GameState.Instance.eventWait)
-        {
-            //Debug.Log(Input.mousePosition + " World Position" + Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-
-            
-        }
-
-        if(Vector2.Distance(transform.position,targetPosition) > 0.3f)
-        {
-            if (!walking)
+        if (!InventoryOpen) {
+            if (Input.GetMouseButtonDown(0) && !GameState.Instance.eventWait)
             {
-                characterAnimator.SetTrigger("Walking");
-                walking = true;
+                //Debug.Log(Input.mousePosition + " World Position" + Camera.main.ScreenToWorldPoint(Input.mousePosition));
+                targetPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+
             }
-            transform.position = Vector2.MoveTowards(transform.position, targetPosition, 1/speed);
-        }else
-        {
-            if (walking)
+
+            if (Vector2.Distance(transform.position, targetPosition) > 0.3f)
             {
+                if (!walking)
+                {
+                    characterAnimator.SetTrigger("Walking");
+                    walking = true;
+                }
+                transform.position = Vector2.MoveTowards(transform.position, targetPosition, 1 / speed);
+            } else
+            {
+                if (walking)
+                {
+                    characterAnimator.SetTrigger("Idle");
+                    walking = false;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                OpenInventory();
                 characterAnimator.SetTrigger("Idle");
-                walking = false;
             }
-        }
+
+            }
 	}
+
+
+    public void OpenInventory()
+    {
+        this.Inventory.SetActive(true);
+        this.InventoryOpen = true;
+    }
 }
---
+++ Assets/Game/Scripts/Inventory/InventoryItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class InventoryItem : MonoBehaviour{
+
+    public Image graphic;
+    public bool followMouse;
+    public GameObject mouseobject;
+    private Transform originalPosition;
+	// Use this for initialization
+	void Start () {
+        originalPosition = transform;
+        followMouse = false;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetKeyDown(KeyCode.C)) {
+            followMouse = false;
+            mouseobject.SetActive(false);
+            this.GetComponent<Image>().CrossFadeAlpha(1.0f, 0.25f, false);
+        }
+
+        if (followMouse)
+        {
+            //transform.position = Input.mousePosition;
+            mouseobject.transform.position = new Vector3 (Camera.main.ScreenToWorldPoint(Input.mousePosition).x,Camera.main.ScreenToWorldPoint(Input.mousePosition).y,0);
+        }
+    
+	}
+
+
+    public void FollowMouse()
+    {
+        Debug.Log("soemthing");
+        mouseobject.SetActive(true);
+        this.GetComponent<Image>().CrossFadeAlpha(0.5f,0.25f,false);
+        followMouse = true;
+    }
+
+
+}
