3c8b883d83943a84f8469936dfc17a1dba03febf
Manuel S. Yepez <manyepez@hotmail.com>
2018-01-27 21:59:24 -0600

    Dialog panel appears / disappears

--- Assets/Game/Scripts/Dialog/DialogManager.cs
+++ Assets/Game/Scripts/Dialog/DialogManager.cs
@@ -9,15 +9,20 @@ public class DialogManager : MonoBehaviour {
     public Text nameText;
     public Text dialogText;
 
+    private GameObject textPanel;
     private Queue<string> sentences;
 
 	// Use this for initialization
 	void Start () {
         sentences = new Queue<string>();
+        textPanel = GameObject.Find("/Main Camera/Canvas/DialogPanel");
 	}
 
     public void StartDialog(Dialog dialog)
     {
+        Image imagePanel = textPanel.GetComponent<Image>();
+        imagePanel.enabled = true;
+        
         nameText.text = dialog.name;
 
         sentences.Clear();
@@ -34,10 +39,17 @@ public class DialogManager : MonoBehaviour {
     {
         if (sentences.Count == 0)
         {
+            EndOfDialog();
             return;
         }
 
         string sentence = sentences.Dequeue();
         dialogText.text = sentence;
+    }
+
+    private void EndOfDialog()
+    {
+        Image imagePanel = textPanel.GetComponent<Image>();
+        imagePanel.enabled = false;
     }
 }
